b635f68017067c0b9a81634ddbd9994b9de057d7
Daniel Demidko <demidko@farpost.com>
2020-06-23 22:50:13 +1000

    Обработка аргументов командной строки

--- CleanMyVk/Program.cs
+++ CleanMyVk/Program.cs
@@ -1,22 +1,58 @@
 ﻿using System;
 using static System.Console;
 using System.Threading.Tasks;
+using static System.Environment;
+using static System.IO.File;
 
 static class Program
 {
+    private const string CachePath = ".cache";
 
-    static async Task Main()
+    private static async Task Main(string[] args)
     {
+        var (login, password) = LoadLoginAndPassword(args);
         
     }
 
-    /*static Vkontakte connectVkApi()
+    private static (string Login, string Password) LoadLoginAndPassword(string[] args) => args.Length switch
     {
-        var vk = new Vkontakte(5984535);
-        vk.AccessToken = AccessToken.FromString(
-            "4e7295590f1533b5d3c270102d1a392573cf75f54089b345864c48f807a5943de248d902aeaf78d42828f"
-        );
-        return vk;
-    }*/
-}
+        0 => LoadLoginAndPasswordFromCache(),
+        2 => LoadLoginAndPasswordFromArgs(args),
+        _ => ExitWithMessage<(string, string)>("First usage:\ndotnet CleanMyVk [login] [password]")
+    };
 
+
+    private static (string Login, string Password) LoadLoginAndPasswordFromArgs(string[] secretArgs)
+    {
+        WriteAllLines(CachePath, secretArgs);
+        return (secretArgs[0], secretArgs[1]);
+    }
+
+    private static (string Login, string Password) LoadLoginAndPasswordFromCache()
+    {
+        if (!Exists(CachePath))
+        {
+            ExitWithMessage(".cache file was not found");
+        }
+
+        var lines = ReadAllLines(CachePath);
+        if (lines.Length != 2)
+        {
+            ExitWithMessage("Broken .cache file");
+        }
+
+        return (lines[0], lines[1]);
+    }
+
+    private static T ExitWithMessage<T>(string message) where T : new()
+    {
+        ExitWithMessage(message);
+        return new T();
+    }
+
+    private static void ExitWithMessage(string message)
+    {
+        WriteLine(message);
+        Exit(1);
+    }
+}
