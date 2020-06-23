0414c64f9fc721a8553d12ea67d983cbaa883e5b
Daniel Demidko <DanielDemidko@gmail.com>
2020-06-23 19:27:34 +1000

    Add files via upload

---
+++ CleanMyVk/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using System.Threading.Tasks;
+
+static class Program
+{
+
+    static async Task Main()
+    {
+        
+    }
+
+    /*static Vkontakte connectVkApi()
+    {
+        var vk = new Vkontakte(5984535);
+        vk.AccessToken = AccessToken.FromString(
+            "4e7295590f1533b5d3c270102d1a392573cf75f54089b345864c48f807a5943de248d902aeaf78d42828f"
+        );
+        return vk;
+    }*/
+}
+
