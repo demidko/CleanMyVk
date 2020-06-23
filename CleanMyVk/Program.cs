dd6cef03a2ef2b567780cef15b21e5240c6d9d45
Daniel Demidko <demidko@farpost.com>
2020-06-23 22:58:18 +1000

    Немножко фп

--- CleanMyVk/Program.cs
+++ CleanMyVk/Program.cs
@@ -10,39 +10,26 @@ static class Program
 
     private static async Task Main(string[] args)
     {
-        var (login, password) = LoadLoginAndPassword(args);
-        
+        var (login, password) = args.LoadLoginAndPassword();
     }
 
-    private static (string Login, string Password) LoadLoginAndPassword(string[] args) => args.Length switch
+    private static (string Login, string Password) LoadLoginAndPassword(this string[] args) => args.Length switch
     {
         0 => LoadLoginAndPasswordFromCache(),
-        2 => LoadLoginAndPasswordFromArgs(args),
+        2 => LoadLoginAndPasswordFrom(args),
         _ => ExitWithMessage<(string, string)>("First usage:\ndotnet CleanMyVk [login] [password]")
     };
 
-
-    private static (string Login, string Password) LoadLoginAndPasswordFromArgs(string[] secretArgs)
+    private static (string Login, string Password) LoadLoginAndPasswordFrom(string[] args)
     {
-        WriteAllLines(CachePath, secretArgs);
-        return (secretArgs[0], secretArgs[1]);
+        WriteAllLines(CachePath, args);
+        return (args[0], args[1]);
     }
 
-    private static (string Login, string Password) LoadLoginAndPasswordFromCache()
-    {
-        if (!Exists(CachePath))
-        {
-            ExitWithMessage(".cache file was not found");
-        }
-
-        var lines = ReadAllLines(CachePath);
-        if (lines.Length != 2)
-        {
-            ExitWithMessage("Broken .cache file");
-        }
-
-        return (lines[0], lines[1]);
-    }
+    private static (string Login, string Password) LoadLoginAndPasswordFromCache() =>
+        (Exists(CachePath) && ReadAllLines(CachePath) is var lines && lines.Length == 2)
+            ? (lines[0], lines[1])
+            : ExitWithMessage<(string, string)>(".cache file broken or wasn't found");
 
     private static T ExitWithMessage<T>(string message) where T : new()
     {
