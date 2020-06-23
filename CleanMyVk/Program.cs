89b3a7561598cc851e3c4d45fc817b1995b47828
Daniel Demidko <demidko@farpost.com>
2020-06-24 01:00:23 +1000

    обходим все новости

--- CleanMyVk/Program.cs
+++ CleanMyVk/Program.cs
@@ -3,24 +3,42 @@ using System.Linq;
 using static System.Console;
 using System.Threading.Tasks;
 using VkNet;
+using VkNet.Enums.Filters;
 using VkNet.Model;
+using VkNet.Model.RequestParams;
 using static System.Environment;
 using static System.UInt32;
 using static System.IO.File;
 
 static class Program
 {
-    /**
-     * Здесь закешированы две строки: логин и пароль
-     */
+    /// Здесь закешированы две строки: логин и пароль
     private const string CachePath = ".user";
 
-    /**
-     * Здесь хранится одна строка app id
-     */
+    /// Здесь хранится одна строка app id
     private const string AppIdPath = ".app";
 
-    private static async Task Main(string[] args)
+    private static void Main(string[] args)
+    {
+        var api = args.LoadVkApi();
+        var filter = new NewsFeedGetCommentsParams
+        {
+            
+        };
+        api.NewsFeed.GetComments().
+        foreach (var i in api.NewsFeed.GetComments(filter).Items)
+        {
+            WriteLine($"{i.Date}({i.Type}): {i.Text}");
+        }
+    }
+    
+    private static async Task<IEquatable<NewsItem>> ListAllNews(VkApi api)
+    {
+        var pageParams = new NewsFeedGetCommentsParams() { }
+        var dataArray = (await api.NewsFeed.GetCommentsAsync(pageParams)).
+    }
+
+    private static VkApi LoadVkApi(this string[] args)
     {
         var api = new VkApi();
         var (login, password) = args.LoadLoginAndPassword();
@@ -28,9 +46,11 @@ static class Program
         {
             ApplicationId = LoadAppId(),
             Login = login,
-            Password = password
+            Password = password,
+            Settings = Settings.All
         });
-        Console.WriteLine(api.Token);
+        WriteLine(api.Token);
+        return api;
     }
 
     private static ulong LoadAppId() =>
@@ -39,14 +59,14 @@ static class Program
         lines.Length == 1 &&
         TryParse(lines.First(), out var appId)
             ? appId
-            : ExitWithMessage<ulong>("Fatal error: .app file (contains app id only) broken or wasn't found");
+            : ExitWithMessage<ulong>($"Fatal error: {AppIdPath} file (contains app id only) broken or wasn't found");
 
     private static (string Login, string Password) LoadLoginAndPassword(this string[] args) => args.Length switch
     {
         0 => LoadLoginAndPasswordFromCache(),
         2 => LoadLoginAndPasswordFrom(args),
         _ => ExitWithMessage<(string, string)>(
-            "usage:\n  With login: dotnet CleanMyVk [login] [password]\n  With cache: dotnet CleanMyVk")
+            "Usage:\n  With login: dotnet CleanMyVk [login] [password]\n  With cache: dotnet CleanMyVk")
     };
 
     private static (string Login, string Password) LoadLoginAndPasswordFrom(string[] args)
@@ -58,7 +78,7 @@ static class Program
     private static (string Login, string Password) LoadLoginAndPasswordFromCache() =>
         Exists(CachePath) && ReadAllLines(CachePath) is var lines && lines.Length == 2
             ? (lines[0], lines[1])
-            : ExitWithMessage<(string, string)>(".cache file broken or wasn't found");
+            : ExitWithMessage<(string, string)>($"{CachePath} file broken or wasn't found");
 
     private static T ExitWithMessage<T>(string message) where T : new()
     {
