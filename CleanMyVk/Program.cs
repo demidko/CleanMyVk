270dc32cf41da7c0115484c097594e679c8924e3
Daniel Demidko <demidko@farpost.com>
2020-06-25 03:11:07 +1000

    gitignore

--- CleanMyVk/Program.cs
+++ CleanMyVk/Program.cs
@@ -1,29 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using static System.Console;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using VkNet;
-using VkNet.Abstractions;
+using VkNet.AudioBypassService.Extensions;
 using VkNet.Enums.Filters;
-using VkNet.Enums.SafetyEnums;
 using VkNet.Model;
-using VkNet.Model.RequestParams;
+using static System.Console;
 using static System.Environment;
 using static System.UInt32;
 using static System.IO.File;
-using VkNet.AudioBypassService.Extensions;
 
-using System;
-using Microsoft.Extensions.DependencyInjection;
-using VkNet;
-using VkNet.Abstractions;
-using VkNet.AudioBypassService.Extensions;
-using VkNet.Model;
-using VkNet.Model.RequestParams;
-
-static class Program
+internal static class Program
 {
     /// Здесь закешированы две строки: логин и пароль
     private const string CachePath = ".user";
@@ -33,21 +20,19 @@ static class Program
 
     private static void Main(string[] args)
     {
-        var api = args.LoadVkApi();
+        var api = InitVkApi(args);
         WriteLine($"Login as vk.com/{api.Account.GetProfileInfo().ScreenName}");
         var conversationsList =
-            api.Messages.GetConversationsById(new List<long>() {186068693});
+            api.Messages.GetConversationsById(new List<long> {186068693});
         WriteLine(conversationsList.Count);
     }
 
-
-    private static VkApi LoadVkApi(this string[] args)
+    private static VkApi InitVkApi(string[] args)
     {
-        
         var services = new ServiceCollection();
         services.AddAudioBypass();
         var api = new VkApi(services);
-        var (login, password) = args.LoadLoginAndPassword();
+        var (login, password) = LoadLoginAndPassword(args);
         api.Authorize(new ApiAuthParams
         {
             ApplicationId = LoadAppId(),
@@ -58,21 +43,27 @@ static class Program
         return api;
     }
 
-    private static ulong LoadAppId() =>
-        Exists(AppIdPath) &&
-        ReadAllLines(AppIdPath) is var lines &&
-        lines.Length == 1 &&
-        TryParse(lines.First(), out var appId)
+
+    private static ulong LoadAppId()
+    {
+        return Exists(AppIdPath) &&
+               ReadAllLines(AppIdPath) is var lines &&
+               lines.Length == 1 &&
+               TryParse(lines.First(), out var appId)
             ? appId
             : ExitWithMessage<ulong>($"Fatal error: {AppIdPath} file (contains app id only) broken or wasn't found");
+    }
 
-    private static (string Login, string Password) LoadLoginAndPassword(this string[] args) => args.Length switch
+    private static (string Login, string Password) LoadLoginAndPassword(string[] args)
     {
-        0 => LoadLoginAndPasswordFromCache(),
-        2 => LoadLoginAndPasswordFrom(args),
-        _ => ExitWithMessage<(string, string)>(
-            "Usage:\n  With login: dotnet CleanMyVk [login] [password]\n  With cache: dotnet CleanMyVk")
-    };
+        return args.Length switch
+        {
+            0 => LoadLoginAndPasswordFromCache(),
+            2 => LoadLoginAndPasswordFrom(args),
+            _ => ExitWithMessage<(string, string)>(
+                "Usage:\n  With login: dotnet CleanMyVk [login] [password]\n  With cache: dotnet CleanMyVk")
+        };
+    }
 
     private static (string Login, string Password) LoadLoginAndPasswordFrom(string[] args)
     {
@@ -80,10 +71,12 @@ static class Program
         return (args[0], args[1]);
     }
 
-    private static (string Login, string Password) LoadLoginAndPasswordFromCache() =>
-        Exists(CachePath) && ReadAllLines(CachePath) is var lines && lines.Length == 2
+    private static (string Login, string Password) LoadLoginAndPasswordFromCache()
+    {
+        return Exists(CachePath) && ReadAllLines(CachePath) is var lines && lines.Length == 2
             ? (lines[0], lines[1])
             : ExitWithMessage<(string, string)>($"{CachePath} file broken or wasn't found");
+    }
 
     private static T ExitWithMessage<T>(string message) where T : new()
     {
