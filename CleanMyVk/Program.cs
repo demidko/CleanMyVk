0d2ef2cfdb88dd9728ded95091fae711002d0e55
Daniel Demidko <demidko@farpost.com>
2020-06-28 23:44:42 +1000

    Структура

--- CleanMyVk/Program.cs
+++ CleanMyVk/Program.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using VkNet;
 using VkNet.AudioBypassService.Extensions;
+using VkNet.Enums;
 using VkNet.Enums.Filters;
 using VkNet.Model;
+using VkNet.Model.RequestParams;
 using static System.Console;
 using static System.Environment;
 using static System.UInt32;
@@ -18,16 +20,19 @@ internal static class Program
     /// Здесь хранится одна строка app id
     private const string AppIdPath = ".app";
 
-    private static void Main(string[] args)
+    private static void Main(string[] args) => Login(args).CleanMyComments().CleanMyLikes();
+
+    private static VkApi CleanMyComments(this VkApi api)
     {
-        var api = InitVkApi(args);
-        WriteLine($"Login as vk.com/{api.Account.GetProfileInfo().ScreenName}");
-        var conversationsList =
-            api.Messages.GetConversationsById(new List<long> {186068693});
-        WriteLine(conversationsList.Count);
+        return api;
     }
 
-    private static VkApi InitVkApi(string[] args)
+    private static VkApi CleanMyLikes(this VkApi api)
+    {
+        return api;
+    }
+
+    private static VkApi Login(string[] args)
     {
         var services = new ServiceCollection();
         services.AddAudioBypass();
@@ -40,43 +45,39 @@ internal static class Program
             Password = password,
             Settings = Settings.All
         });
+        WriteLine($"Login as vk.com/{api.Account.GetProfileInfo().ScreenName}");
         return api;
     }
-
-
-    private static ulong LoadAppId()
-    {
-        return Exists(AppIdPath) &&
-               ReadAllLines(AppIdPath) is var lines &&
-               lines.Length == 1 &&
-               TryParse(lines.First(), out var appId)
+    
+    private static ulong LoadAppId() =>
+        Exists(AppIdPath) &&
+        ReadAllLines(AppIdPath) is var lines &&
+        lines.Length == 1 &&
+        TryParse(lines.First(), out var appId)
             ? appId
             : ExitWithMessage<ulong>($"Fatal error: {AppIdPath} file (contains app id only) broken or wasn't found");
+
+
+    private static (string Login, string Password) LoadLoginAndPassword(string[] args) => args.Length switch
+    {
+        0 => LoadLoginAndPasswordFromCache(),
+        2 => LoadLoginAndPasswordFromInput(args),
+        _ => ExitWithMessage<(string, string)>(
+            "Usage:\n  With login: dotnet CleanMyVk [login] [password]\n  With cache: dotnet CleanMyVk")
+    };
+
+
+    private static (string Login, string Password) LoadLoginAndPasswordFromInput(string[] input)
+    {
+        WriteAllLines(CachePath, input);
+        return (input[0], input[1]);
     }
 
-    private static (string Login, string Password) LoadLoginAndPassword(string[] args)
-    {
-        return args.Length switch
-        {
-            0 => LoadLoginAndPasswordFromCache(),
-            2 => LoadLoginAndPasswordFrom(args),
-            _ => ExitWithMessage<(string, string)>(
-                "Usage:\n  With login: dotnet CleanMyVk [login] [password]\n  With cache: dotnet CleanMyVk")
-        };
-    }
-
-    private static (string Login, string Password) LoadLoginAndPasswordFrom(string[] args)
-    {
-        WriteAllLines(CachePath, args);
-        return (args[0], args[1]);
-    }
-
-    private static (string Login, string Password) LoadLoginAndPasswordFromCache()
-    {
-        return Exists(CachePath) && ReadAllLines(CachePath) is var lines && lines.Length == 2
+    private static (string Login, string Password) LoadLoginAndPasswordFromCache() =>
+        Exists(CachePath) && ReadAllLines(CachePath) is var lines && lines.Length == 2
             ? (lines[0], lines[1])
             : ExitWithMessage<(string, string)>($"{CachePath} file broken or wasn't found");
-    }
+
 
     private static T ExitWithMessage<T>(string message) where T : new()
     {
