150c93053cbda21af2c8d873371c9a234b8521c8
Daniel Demidko <demidko@farpost.com>
2020-10-11 20:56:05 +1000

    Selenium

--- src/Application.cs
+++ src/Application.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using Microsoft.VisualBasic;
+using OpenQA.Selenium.Firefox;
 using static System.ConsoleColor;
 using static System.IO.Directory;
 using static Authorization;
@@ -13,6 +15,9 @@ internal static class Application
 {
     private static void Main(string[] args)
     {
-        
+        using var driver = new FirefoxDriver();
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl("https://google.com");
+        Thread.Sleep(TimeSpan.FromSeconds(30));
     }
 }
