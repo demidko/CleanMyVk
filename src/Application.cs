f545878c82fa73268f509c15d0a4d6958e0642c3
Daniel Demidko <demidko@farpost.com>
2020-10-11 21:31:34 +1000

    Добавил поддержку Chrome

--- src/Application.cs
+++ src/Application.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Threading;
 using Microsoft.VisualBasic;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
 using static System.ConsoleColor;
 using static System.IO.Directory;
+using static System.TimeSpan;
 using static Authorization;
+using static OpenQA.Selenium.By;
 
 /// <summary>
 /// Точка входа описывает логику приложения на высоком уровне
@@ -15,9 +18,13 @@ internal static class Application
 {
     private static void Main(string[] args)
     {
-        using var driver = new FirefoxDriver();
+        
+        using var driver = new ChromeDriver();
         driver.Manage().Window.Maximize();
         driver.Navigate().GoToUrl("https://google.com");
-        Thread.Sleep(TimeSpan.FromSeconds(30));
+        var el = driver.FindElement(Name("q"));
+        el.SendKeys("search info about selenium");
+        el.Submit();
+        Thread.Sleep(FromSeconds(10));
     }
 }
